1cbe67d0855907292f6e376d37dff0347b767c87
Navi-Beep <estawars30@gmail.com>
2022-09-26 21:20:29 -0500

    added bird

---
+++ src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import cockatiel2 from './cockatiel2.png'
+
+
+export default function Navbar(props){
+    return(
+        <nav class="navbar navbar-expand-lg bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="/">Elise's Portfolio</a>
+                <img src={cockatiel2} class="cockatiel" alt="round birb" />
+                <button class="navbar-toggler border border-1" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link active" aria-current="page" href="/">Home</a>
+                    <a class="nav-link" href="/">Features</a>
+                    <a class="nav-link" href="/">Pricing</a>
+                </div>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+//cockatiel - <a href="https://www.flaticon.com/free-icons/cockatiel" title="cockatiel icons">Cockatiel icons created by Freepik - Flaticon</a>
