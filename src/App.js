e086aa6003d9db4d9f21d29258ed28cab7174a90
Navi-Beep <estawars30@gmail.com>
2022-10-01 22:29:31 -0500

    added more text, need components

--- src/App.js
+++ src/App.js
@@ -7,6 +7,19 @@ function App() {
       <Navbar name='Elise'/>
       <div>
       <h2 className='text-center'>Project Portfolio</h2>
+      <h3 className='text-center'>2022</h3>
+      </div>
+
+      <div>
+      <h2 className='text-center'>Cat-Tastic Fantastic App</h2>
+      </div>
+
+      <div>
+      <h2 className='text-center'>Gopher Weather API</h2>
+      </div>
+
+      <div>
+      <h2 className='text-center'>Address Book</h2>
       </div>
     
       </>
