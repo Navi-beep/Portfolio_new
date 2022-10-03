e00dbc97f7a1e451f4325b5878e86157f171d6dd
Navi-Beep <estawars30@gmail.com>
2022-10-02 20:59:36 -0500

    added navbar links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,19 +4,19 @@ import cockatiel2 from './cockatiel2.png'
 
 export default function Navbar(props){
     return(
-        <nav class="navbar navbar-expand-lg bg-light">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="/
-                "><img src={cockatiel2} class="cockatiel" alt="round birb" />Elise's Portfolio</a>
+        <nav className="navbar navbar-expand-lg bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="/
+                "><img src={cockatiel2} className="cockatiel" alt="round birb" />Elise's Portfolio</a>
                 
-                <button class="navbar-toggler border border-1" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+                <button className="navbar-toggler border border-1" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
-                    <a class="nav-link active" aria-current="page" href="/">Home</a>
-                    <a class="nav-link" href="/">LinkedIn</a>
-                    <a class="nav-link" href="/">Github</a>
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="navbar-nav">
+                    <a className="nav-link active" aria-current="page" href="/">Home</a>
+                    <a className="nav-link" href="https://www.linkedin.com/in/elise-stawarski-a7698028/" target="_blank">LinkedIn</a>
+                    <a className="nav-link" href="https://github.com/Navi-beep" target="_blank">Github</a>
                 </div>
                 </div>
             </div>
